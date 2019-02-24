ff5b336944acfe8f230acd90bd0b02aacf60e5a7
lihaodong <lihaodongmail@163.com>
2019-02-24 21:41:23 +0800

    第十一篇：Spring Boot中使用JavaMailSender发送邮件
    第十一篇：Spring Boot中使用JavaMailSender发送邮件

---
+++ springboot-mail/src/main/java/com/li/springbootmail/SpringbootMailApplication.java
@@ -0,0 +1,13 @@
+package com.li.springbootmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMailApplication.class, args);
+    }
+
+}
